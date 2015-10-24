282f030d4488ff339129bebbedb5cc088557dd94
swacky <student@Worlds-1.local>
2015-10-24 20:29:27 +1100

    with oculus

---
+++ Assets/test.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO; 
+
+public class test : MonoBehaviour {
+
+	public float speed = 12f;
+	float mySpeed =0;
+	Camera cam;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		if(Input.GetKeyDown(KeyCode.F ))
+			mySpeed += speed;
+		if (Input.GetKeyDown (KeyCode.B) )
+			mySpeed -= speed;
+		if (Input.GetKeyDown (KeyCode.Alpha0) ) {
+			//print ("whatever");
+			mySpeed = 0;
+		}
+		//		if (Input.GetAxis("Vertical") > 0)
+		//		{
+		//			transform.Translate(0f,0f,0.1f);
+		//		}
+		GetComponent<Rigidbody>().AddForce (Input.GetAxis("Vertical") * speed * transform.forward);
+		//transform.Rotate (Vector3.up * Time.deltaTime * Input.GetAxis("Vertical"));
+		//transform.Rotate (Input.GetAxis("Vertical"),0,0);
+		//transform.Rotate (0,Input.GetAxis("Horizontal"),0);
+		GetComponent<Rigidbody>().velocity= (mySpeed * transform.forward);
+	}
+}
